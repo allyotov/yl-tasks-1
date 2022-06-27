6577927f5af58093ded9d236b982e5fcffdfde5b
Alexander Lyotov <sippie84@gmail.com>
2022-06-27 12:16:35 +0300

    feature/task 5 cont_find_num refactored

--- task_5.py
+++ task_5.py
@@ -1,73 +1,21 @@
-import logging
-
-logging.basicConfig(level=logging.INFO)
-
-
-def multiply_list(numbers_list):
-    result = 1
-    for element in numbers_list:
-        result *= element
-    return result
-
-def count_find_num(primesL, limit):
-    mults = set()
-    first_mult = multiply_list(primesL)
-    greatest = first_mult
-    if first_mult < limit:
-        mults.add(first_mult)
-    elif first_mult == limit:
-        return [1, first_mult]
-    else:
-        logging.debug('no way')
+def get_numbers(base, primesL, n):
+    if(base > n):
         return []
-
-    logging.debug('first mult %s' % first_mult)
-    logging.debug('greatest %s' % greatest)
-
-    initial_copy = primesL[:]
-    el = 0
-    pow = 1
-    while multiply_list(primesL) < limit:
-        for i, element in enumerate(primesL):
-            degree = 1
-            multiplication = 0
-            while True:
-                prime_list_copy = primesL[:]
-                prime_list_copy[i] = element * initial_copy[i] ** degree
-                logging.debug('current list [%s]' % ', '.join([str(element) for element in prime_list_copy]))
-                multiplication = multiply_list(prime_list_copy)
-                logging.debug('Multiplication: %s' % multiplication)
-                if multiplication <= limit:
-                    mults.add(multiplication)
-                    if multiplication > greatest:
-                        greatest = multiplication
-                    degree += 1
-                else:
-                    break
-        logging.debug('\n\n\n')
-        primesL = initial_copy[:]
-        primesL[el] *= initial_copy[el] ** pow
-        el += 1
-        if el > len(primesL) - 1:
-            el = 0
-            pow += 1
-
-    extended = True
-    while extended:
-        extended = False
-        for mult in initial_copy:
-            for old_mult in list(mults):
-                new_mult = mult * old_mult
-                if new_mult not in mults and new_mult <= limit:
-                    extended = True
-                    mults.add(new_mult)
-                    if greatest < new_mult:
-                        greatest = new_mult
+    numbers = [base]
+    for p in primesL:
+        numbers += get_numbers(base * p, primesL, n)   
+    return list(set(numbers))
     
-    logging.debug('numbers [%s]' % ', '.join([str(element) for element in sorted(list(mults))]))
-    logging.debug('count %s' % len(mults))
-    logging.debug('greatest %s' % greatest)
-    return [len(mults), greatest]
+
+def count_find_num(primesL, n):
+    base = 1
+    for p in primesL:
+        base *= p
+    
+    numbers = get_numbers(base, primesL, n)
+    if(len(numbers) == 0):
+        return []
+    return [len(numbers), max(numbers)]
 
 
 if __name__ == '__main__':
@@ -95,4 +43,3 @@ if __name__ == '__main__':
     primesL = [2, 3, 47]
     limit = 200
     assert count_find_num(primesL, limit) == []
-    logging.info('All tests passed!')
