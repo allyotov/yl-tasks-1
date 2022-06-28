2e9c0fa8832f5091e8f94a41b48eb1054107a934
Alexander Lyotov <sippie84@gmail.com>
2022-06-28 08:49:09 +0300

    feature/task_3 refactor

--- task_3.py
+++ task_3.py
@@ -1,10 +1,12 @@
 def zeros(n):
-    n_zeros = 0
-    while(n >= 5):
-        n //= 5
-        n_zeros += n
-    return n_zeros
-
+    pow_of_5 = 5
+    zeros = 0
+    
+    while n >= pow_of_5:
+        zeros += n // pow_of_5
+        pow_of_5 *= 5
+        
+    return zeros
 
 if __name__ == '__main__':
     assert zeros(0) == 0
