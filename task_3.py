074ddafff8df3f7fd7e7e9f56bb8bcb0de052679
Alexander Lyotov <sippie84@gmail.com>
2022-06-28 11:26:02 +0300

    feature/task 3 debug

--- task_3.py
+++ task_3.py
@@ -1,14 +1,21 @@
+import logging
+logging.basicConfig(level=logging.DEBUG)
+
 def zeros(n):
     pow_of_5 = 5
     zeros = 0
-    
+    iter = 0
     while n >= pow_of_5:
+        iter += 1
         zeros += n // pow_of_5
         pow_of_5 *= 5
-        
+    logging.debug('Исходное число n = %s, количество итераций цикла: %s' % (n, iter))
     return zeros
+
 
 if __name__ == '__main__':
     assert zeros(0) == 0
     assert zeros(6) == 1
     assert zeros(30) == 7
+    assert zeros(633922932) == 158480726
+    assert zeros(703594007) == 175898498
